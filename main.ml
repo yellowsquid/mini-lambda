73a5f839b40f0a02cbc13e237d7bb2892b1a3086
Greg Brown <greg.brown.00@outlook.com>
2020-01-18 16:49:27 +0000

    Make error messages more consistent.

--- main.ml
+++ main.ml
@@ -49,9 +49,8 @@ let () =
       (Lexing.lexeme lexbuf);
     exit 1
   | Typing.Error(pos, msg) ->
-    Printf.eprintf "(%d, %d) type error: %s\n"
+    Printf.eprintf "(%d:%d) type error: %s\n"
       pos.Lexing.pos_lnum
       (pos.Lexing.pos_cnum - pos.Lexing.pos_bol + 1)
       msg;
     exit 1
-
